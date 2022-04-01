289235853e51b6ced2e99f3f57f0a1ce26aa6699
yamen <191036@ppu.edu.ps>
2022-04-02 00:05:16 +0300

    move user_login.php to the Pages directory

---
+++ Pages/user_login.php
@@ -0,0 +1,22 @@
+<?php
+    // require "../Back_End/configure_database.php";
+    // require "./Back_End/check_user.php";
+    require_once "../models/login.php";
+    session_start();
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    
+    login($username, $password);
+
+    // $getUsername = "Select username, password from users";
+    // $result = mysqli_query($conn, $getUsername);
+    
+    // if (checkUser($username, $password, $result) == 1) {
+    //     //go to home page
+    //     echo "now you must in the home page";
+    // } else {
+    //     echo '<script>alert("Wrong Username or Password")</script>';
+    // }
+    
+?>
