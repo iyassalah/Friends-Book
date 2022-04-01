e8b519dd60a287a24de4baa9f02817b152632988
iyas salah <191034@ppu.edu>
2022-04-01 20:14:36 +0300

    ready login

--- Back_End/configure_database.php
+++ Back_End/configure_database.php
@@ -1,4 +1,13 @@
 <?php
+    $servername = "127.0.0.1";
+    $username = "admin";
+    $password = "iyas";
+    $dbname = "frinds_book";
     
-
+    $conn = mysqli_connect($servername, $username, $password,$dbname);
+    
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+     
 ?>
---
+++ Back_End/user_login.php
@@ -0,0 +1,18 @@
+<?php
+    require "./configure_database.php";
+    require "./check_user.php";
+    
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    
+    $getUsername = "Select username, password from users";
+    $result = mysqli_query($conn, $getUsername);
+    
+    if (checkUser($username, $password, $result) == 1) {
+        //go to home page
+        echo "sucess";
+    } else {
+        echo '<script>alert("Wrong Username or Password")</script>';
+    }
+    
+?>
