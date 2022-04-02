5e0c2fc9c51b5504ec7fb00985e6a032494b0026
yamen <191036@ppu.edu.ps>
2022-04-02 15:49:48 +0300

    added regUser function and file

---
+++ models/reg-user.php
@@ -0,0 +1,50 @@
+<?php
+require_once '../helpers/db.php';
+require_once '../helpers/check-duplicates.php';
+/**
+ * @return true if the registration was successful
+ */
+function regUser(array $data)
+{
+    [
+        "firstname" => $fname,
+        "lastname" => $lname,
+        "username" => $username,
+        "email" => $email,
+        "password" => $password,
+        "flexRadioDefault" => $gender,
+        "tele-number" => $phone,
+        "address" => $address,
+        // "image" => $image,
+    ] = $data;
+
+    if (checkDuplicate($username, $email)) {
+        require_once '../templates/error-message.php';
+        showError("Username or email taken");
+        return false;
+    }
+    $query = "INSERT INTO `users`(
+    `email`,
+    `username`,
+    `password`,
+    `phone`,
+    `date_joined`,
+    `address`,
+    `gender`,
+    `fname`,
+    `lname`
+)
+VALUES(
+    '$email',
+    '$username',
+    '$password',
+    '$phone',
+    CURRENT_TIMESTAMP(), '$address', '$gender', '$fname', '$lname');";
+
+
+    try {
+        return mysqli_query(db(), $query);
+    } catch (\Throwable $th) {
+        echo "data base errror";
+    }
+}
