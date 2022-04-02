04d7bf7a1d8f43d0d2e02dfdd8a71ca3e111756e
yamen <191036@ppu.edu.ps>
2022-04-02 13:15:38 +0300

    added error message and dynamic CSS loading

--- Pages/login.php
+++ Pages/login.php
@@ -5,13 +5,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="../Styles/loginStyle.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <style>
+        <?php
+        include("..\Styles\loginStyle.css");
+        // echo '<link rel="stylesheet" href="../Styles/loginStyle.css">';
+        ?>
+    </style>
     <title>Login</title>
 </head>
 
 <body>
+    <?php
+    require_once('../templates/error-message.php');
+    showError()
+    ?>
     <div id="login">
         <div class="container">
             <div id="login-row" class="row justify-content-center align-items-center">
