45f3762d033055b85238dd5aeb49121f6add3b8c
yamen <191036@ppu.edu.ps>
2022-04-02 15:51:33 +0300

    added error handling, showError() with import

--- models/add-friend-request.php
+++ models/add-friend-request.php
@@ -3,6 +3,7 @@ function addFriendship(string $user1, string $user2)
 {
     require_once '../helpers/db.php';
     require_once '../templates/success-message.php';
+    require_once '../templates/error-message.php';
     $query = "INSERT INTO `friendships`(
         `friend1_id`,
         `friend2_id`,
@@ -10,7 +11,12 @@ function addFriendship(string $user1, string $user2)
         `accepted`
     )
     VALUES('$user1', '$user2', CURRENT_TIMESTAMP(), '0')";
-    $result = mysqli_query(db(), $query);
+    $result = null;
+    try {
+        $result = mysqli_query(db(), $query);
+    } catch (\Throwable $th) {
+        showError("Database error");
+    }
     if (!$result) {
         showError("could not create friend request");
     } else {
