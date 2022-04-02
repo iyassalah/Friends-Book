6c26cc6086fb2257aab9523650b61d68abcfb2a8
yamen <191036@ppu.edu.ps>
2022-04-02 13:12:41 +0300

    added get recommended friends model

---
+++ models/get-recommended-friends.php
@@ -0,0 +1,25 @@
+<?php
+function getRecommendedFriends(string $uid)
+{
+    $query =
+        "SELECT
+    fname, lname, username, user_id, image
+FROM
+    users
+WHERE
+    users.user_id NOT IN(
+    SELECT CASE WHEN
+        friendships.friend1_id = $uid THEN friendships.friend2_id WHEN friendships.friend2_id = $uid THEN friendships.friend1_id
+END
+FROM
+    friendships
+WHERE
+    friend1_id = $uid OR friend2_id = $uid
+);";
+    require_once('../helpers/db.php');
+    $result = mysqli_query(db(), $query);
+    if ($result)
+    {
+        return $result;
+    }
+}
