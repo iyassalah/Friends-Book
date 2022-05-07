44766129e61e43fd14ea832a4b4caa98309326b8
yamen <191036@ppu.edu.ps>
2022-05-07 13:06:26 +0300

    changed profile to use the CSS

--- templates/profile-page.php
+++ templates/profile-page.php
@@ -3,7 +3,16 @@ function showProfile(string $uid)
 {
     require_once '../models/get-user-profile.php';
     $data = getUserProfile($uid);
-    foreach ($data as $key => $value) {
-        echo $key . ': ' . $value . "<br>";
-    }
+    echo "<div class='name'>";
+    echo $data['fname'] . ' ' . $data['lname'] . " (" . $data['username'] . ")";
+    echo "</div>";
+    echo "<div class='gender'>";
+    echo $data['gender'];
+    echo "</div>";
+    echo "<div class='address'>Lives in ";
+    echo $data['address'];
+    echo "</div>";
+    echo "<div class='date_joined'> Joined on ";
+    echo $data['date_joined'];
+    echo "</div>";
 }
