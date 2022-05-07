e306e6967a0da9539695d43704fef798511eb9aa
yamen <191036@ppu.edu.ps>
2022-05-07 19:26:41 +0300

    added acceptFriend() to set friendship acc to 1

--- models/confirm-friend-request.php
+++ models/confirm-friend-request.php
@@ -13,3 +13,19 @@ function deleteFriend($id1, $id2)
         throw new Exception('Database error');
     }
 }
+
+function acceptFriend($id1, $id2)
+{
+    require_once('../helpers/db.php');
+    $query = "UPDATE friendships SET accepted = 1 WHERE friend2_id = $id2 AND friend1_id = $id1 OR friend2_id = $id1 AND friend1_id = $id2";
+    $result = mysqli_query(db(), $query);
+    if ($result)    
+    {
+        return $result;
+    }
+    else
+    {
+        throw new Exception('Database error');
+    }
+}
+
