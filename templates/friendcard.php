bf0b18934cd19921c34f57eff4dfb11ad9f529f4
yamen <191036@ppu.edu.ps>
2022-05-07 18:43:05 +0300

    added friend card template

---
+++ templates/friendcard.php
@@ -0,0 +1,15 @@
+<?php
+function showFriendCard(array $data, $uid)
+{
+
+    ['fname' => $fname, 'lname' => $lname, 'username' => $username, 'user_id' => $user_id, 'accepted' => $accepted] = $data;
+
+
+    echo "<div class='friend-card'>";
+    echo "<span class='username'> $fname $lname </span>";
+    echo "<div id='confirm'>";
+    echo "<button onclick='sendConfirm(" . $user_id . ", " . $uid . ", true)'> accept </button> ";
+    echo "<button onclick='sendConfirm(" . $user_id . ", " . $uid . ", false)'> deny </button> ";
+    echo "</div>";
+    echo "</div>";
+}
