282deb27b0ced01fa86887395a90cfd8d1b91619
yamen <191036@ppu.edu.ps>
2022-05-07 13:06:44 +0300

    added require_once() for showPost

--- templates/timeline.php
+++ templates/timeline.php
@@ -1,6 +1,7 @@
 <?php
 function timeline(mysqli_result $data)
 {
+    require_once('../templates/post.php');
     echo "<div class='timeline'>";
     while ($row = mysqli_fetch_assoc($data)) {
         showPost($row);
