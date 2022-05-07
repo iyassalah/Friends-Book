5c6d12197fa3949fbdbb5f3019ca160b2bd16cc2
yamen <191036@ppu.edu.ps>
2022-05-07 13:05:02 +0300

    added getUserPosts(),needs to join with user table

--- models/get-user-profile.php
+++ models/get-user-profile.php
@@ -14,3 +14,23 @@ function getUserProfile(string $uid)
         throw $th;
     }
 }
+
+
+function getUserPosts(string $uid)
+{
+    require_once '../templates/error-message.php';
+    $query = "SELECT * FROM `posts` WHERE author_id = $uid;";
+    // echo $query;
+    require_once '../helpers/db.php';
+    $result = null;
+    try {
+        $result = mysqli_query(db(), $query);
+        if (!$result) {
+            throw new Exception('query failed');
+        }
+        return $result;
+    } catch (\Throwable $th) {
+        showError("data base error");
+        throw $th;
+    }
+}
