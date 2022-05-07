2e9c4ccc934430f4a783088910f35e49dec374dc
yamen <191036@ppu.edu.ps>
2022-05-07 13:05:51 +0300

    added css, $uid var, timeline removed debugs

--- Pages/profile.php
+++ Pages/profile.php
@@ -5,22 +5,50 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        <?php
+        include("..\Styles\profile.css");
+        include '../Styles/timeline.css';
+        // echo '<link rel="stylesheet" href="../Styles/loginStyle.css">';
+        ?>
+    </style>
     <title>Document</title>
 </head>
 
 <body>
     <h1>profile page</h1>
     <a href="./homepage.php">homepage</a>
-    <?php
+    <div class="main-container">
+        <?php
+        session_start();
+        require_once '../templates/profile-page.php';
+        $uid = $_SESSION["data"]["user_id"];
+        // echo print_r($_SESSION);
+        if (isset($uid)) {
+            echo 'here';
+            showProfile($uid);
+        }
+        ?>
+        <?php
         session_start();
         require_once '../templates/profile-page.php';
         // echo print_r($_SESSION);
-        if (isset($_SESSION["data"]["user_id"]))
-        {
-            echo 'here';
-            showProfile($_SESSION["data"]["user_id"]);
+        if (isset($uid)) {
+            showProfile($uid);
         }
-    ?>
+        ?>
+    </div>
+    <div class="timeline">
+        <?php
+        // echo print_r($_SESSION);
+        require_once('../models/get-user-profile.php');
+        require_once('../templates/timeline.php');
+        if (isset($uid)) {
+            $posts = getUserPosts($uid);
+            timeline($posts);
+        }
+        ?>
+    </div>
 </body>
 
 </html>
