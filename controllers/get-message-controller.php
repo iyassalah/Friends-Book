e56c9a325364d8730f81f3bcd70afeafb6da8bb0
yamen <191036@ppu.edu.ps>
2022-05-12 00:55:03 +0300

    add API endpoint and cotroller for getMessages

---
+++ controllers/get-message-controller.php
@@ -0,0 +1,23 @@
+<?php
+
+// print_r($_POST);
+header('Content-Type: application/json; charset=utf-8');
+try {
+    if (!(isset($_POST["sender"]) && isset($_POST["recipient"]))) {
+        throw new Exception("Not enough paramters");
+    }
+    $sender = $_POST['sender'];
+    $recipient = $_POST['recipient'];
+    require_once '../models/send-message.php';
+    if ($result = getMessages($sender, $recipient)) {
+        echo json_encode(["success" => true, "data" => $result]);
+    } else {
+        echo json_encode(["success" => false]);
+    }
+} catch (\Throwable $th) {
+    echo json_encode(["success" => false, "msg" => $th->getMessage()]);
+}
+
+/**
+ * THE DATA HAS TO BE SENT IN x-www-form-urlencoded
+ */
