6b0d0c052987f894ccdd4f14db4c104b412ad76a
yamen <191036@ppu.edu.ps>
2022-05-12 00:54:39 +0300

    remove set header line

--- models/send-message.php
+++ models/send-message.php
@@ -2,7 +2,6 @@
 function addMessage(string $sender, string $recipient, string $content)
 {
     require_once '../helpers/db.php';
-    header('Content-Type: application/json; charset=utf-8');
     try {
         $query = "INSERT INTO messages (msg_id, sender_id, recipient_id, content) VALUES (NULL, '$sender', '$recipient', '$content');";
         $result = mysqli_query(db(), $query);
