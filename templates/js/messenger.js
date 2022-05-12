d9888c311f7ee4b445ea96421631e908b070b071
yamen <191036@ppu.edu.ps>
2022-05-12 21:54:38 +0300

    add AJAX request, re order declarations

--- templates/js/messenger.js
+++ templates/js/messenger.js
@@ -1,19 +1,37 @@
-$(document).ready(() => {
-  $.ajax({
-    type: "method",
-    url: "url",
-    data: "data",
-    dataType: "dataType",
-    success: function (response) {},
-  });
-});
-
-function addMessage(msgId, content, sender, timestamp) {
+function addMessage(msgId, content, sender, timestamp, recieved) {
   $("#timeline").prepend(/*html*/ `
-  <div class="msg" id="msg${msgId}">
+  <div class="msg ${recieved ? "inbox" : "sent"}" id="msg${msgId}">
     <span class="sender">${sender}</span>
     <span class="timestamp">${timestamp}</span>
     <div class="msg-content">${content}</div>
   </div>
 `);
 }
+$(document).ready(() => {
+  $.ajax({
+    type: "post",
+    url: `http://localhost:8080/api-get-messages.php`,
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    data: { sender: sender, recipient: recipient },
+    dataType: "json",
+    success: function (response) {
+      console.log(response);
+      for (const msg of response.data) {
+        console.log(msg);
+        console.log(recipient);
+        console.log(msg.sender_id);
+
+        addMessage(
+          msg.msg_id,
+          msg.content,
+          msg.sender_id,
+          msg.timestamp,
+          msg.sender_id == recipient
+        );
+      }
+    },
+    error: function (xhr, exception) {
+      console.error("failed to fetch messages");
+    },
+  });
+});
