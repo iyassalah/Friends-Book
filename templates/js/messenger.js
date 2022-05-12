c6d7b173939958c3976d9ac46e2cd057b84f6755
yamen <191036@ppu.edu.ps>
2022-05-12 22:35:29 +0300

    add send msg via ajax, render msg on response

--- templates/js/messenger.js
+++ templates/js/messenger.js
@@ -1,20 +1,72 @@
 /**
- * render message on the timeline 
+ * render message on the timeline
  * @param {*} msgId globally unique message ID
  * @param {*} content message text content
  * @param {*} sender message author
  * @param {*} timestamp when the message was sent
  * @param { boolean } recieved if this message was recieved or sent from the client
  */
-function addMessage(msgId, content, sender, timestamp, recieved) {// TODO find sensible names for sender and recieved
+function addMessage(msgId, content, sender, timestamp, recieved, failed) {
+  // TODO find sensible names for sender and recieved
   $("#timeline").prepend(/*html*/ `
-  <div class="msg ${recieved ? "inbox" : "sent"}" id="msg${msgId}"> 
+  <div class="msg ${recieved ? "inbox" : "sent"} ${
+    failed ? "failed" : "succeeded"
+  }" id="msg${msgId}"> 
     <span class="sender">${sender}</span>
     <span class="timestamp">${timestamp}</span>
     <div class="msg-content">${content}</div>
   </div>
 `);
 }
+
+function handleSent(response) {
+  addMessage(
+    response.msg_id,
+    response.content,
+    sender,
+    "TIME place holder", // TODO remove placeholder for timestamp
+    false,
+    false
+  );
+}
+
+function postMessage(content) {
+    $.ajax({
+        type: "post",
+        url: `http://localhost:8080/api-send-message.php`,
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        data: { sender: sender, recipient: recipient, content: content },
+        dataType: "json",
+        success: handleSent,
+        error: function (xhr, exception) {
+            console.error("failed to fetch messages");
+        },
+    });
+}
+
+function handleClick() {
+    const input = document.getElementById('in');
+    postMessage(input.value);
+}
+
+function handleFetch(response) {
+  console.log(response);
+  for (const msg of response.data) {
+    console.log(msg);
+    console.log(recipient);
+    console.log(msg.sender_id);
+
+    addMessage(
+      msg.msg_id,
+      msg.content,
+      msg.sender_id,
+      msg.timestamp,
+      msg.sender_id == recipient,
+      false
+    );
+  }
+}
+
 $(document).ready(() => {
   $.ajax({
     type: "post",
@@ -22,22 +74,7 @@ $(document).ready(() => {
     headers: { "Content-Type": "application/x-www-form-urlencoded" }, // set to form data type
     data: { sender: sender, recipient: recipient }, // chat ID, those values must exist before calling this by setting them via PHP
     dataType: "json", // tell jquery to expect JSON, removing this causes it to think that the request failed even if it succeeds
-    success: function (response) {
-      console.log(response);
-      for (const msg of response.data) {
-        console.log(msg);
-        console.log(recipient);
-        console.log(msg.sender_id);
-
-        addMessage(
-          msg.msg_id,
-          msg.content,
-          msg.sender_id,
-          msg.timestamp,
-          msg.sender_id == recipient
-        );
-      }
-    },
+    success: handleFetch,
     error: function (xhr, exception) {
       console.error("failed to fetch messages");
     },
