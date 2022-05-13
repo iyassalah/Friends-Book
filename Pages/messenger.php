50e5422c4bb2eb0586f34dbf5b370650f423f39d
yamen <191036@ppu.edu.ps>
2022-05-13 15:54:31 +0300

    refactor, check if user is logged in else 403

--- Pages/messenger.php
+++ Pages/messenger.php
@@ -6,10 +6,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <?php
+    if (session_id() == '' || !isset($_SESSION) || session_status() === PHP_SESSION_NONE) {
+        session_start();
+    }
+    $sender = $_GET['sender']; // TODO properly fetch chat ID 
+    $recipient = $_GET['recipient'];
+    echo $recipient;
+    echo $sender;
+    echo $_SESSION["data"]["user_id"];
+    if ($sender != $_SESSION["data"]["user_id"] && $recipient != $_SESSION["data"]["user_id"]) {
+        // header("Location: /403.php");
+    }
+    ?>
     <script>
         <?php
-        $sender = $_GET['sender']; // TODO properly fetch chat ID 
-        $recipient = $_GET['recipient'];
         echo "const sender = $sender;"; // TODO properly send in IDs to JS
         echo "const recipient = $recipient;";
         include '../templates/js/messenger.js';
