10532c0f01bb2315f0e4e54f5d26abde42942832
iyas salah <191034@ppu.edu>
2022-05-15 00:15:55 +0300

    style the new post page

--- Pages/postform.php
+++ Pages/postform.php
@@ -4,14 +4,41 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather+Sans:wght@500&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/7f83ce35c1.js" crossorigin="anonymous"></script>
     <title>New Post</title>
+    <style>
+        <?php
+        include '../Styles/navbar.css';
+        include '../Styles/newpost.css';
+        include '../Styles/standards.css';
+        ?>
+    </style>
 </head>
-<body>
+<body class="full-hight">
 
-    <?php
-    // include '../Style/'
-    include '../templates/postform.php';
-    ?>
+    <nav class="mynav">
+        <div class="nav-container">
+            <div class="nav-part">
+                <a class="nav-font logo" href="./homepage.php">FriendsBook</a>
+            </div>
+            <div>
+                <a class="nav-font white-font logo" href=" ./profile.php"><i class="fa-solid fa-user white-font"></i>Profile page</a>
+            </div>
+        </div>
+    </nav>
+
+    <section class="main-section">
+        <?php
+        include '../templates/postform.php';
+        ?>
+    </section>
+
     
 </body>
 </html>
--- Pages/register.php
+++ Pages/register.php
@@ -6,12 +6,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="../Styles/registerStyle.css">
+    <!-- <link rel="stylesheet" href="../Styles/registerStyle.css"> -->
     <title>Register</title>
     <?php
-
     require_once '../controllers/register-controller.php';
     ?>
+    <style>
+        <?php
+        include '../Styles/registerStyle.css';
+        ?>
+    </style>
 </head>
 
 <body>
