43dee96a24370d41190b4894140af0556de78023
iyas salah <191034@ppu.edu>
2022-05-14 22:26:32 +0300

    style the data section in profile

--- templates/friendcard.php
+++ templates/friendcard.php
@@ -5,17 +5,17 @@ function showFriendCard(array $data, $uid)
     ['fname' => $fname, 'lname' => $lname, 'username' => $username, 'user_id' => $user_id, 'accepted' => $accepted] = $data;
 
 
-    echo "<div class='friend-card d-flex justify-content-between'>";
-    echo "<span class='d-flex justify-content-between'> $fname $lname </span>";
-    echo "<div class='d-flex justify-content-between' id='confirm" . $user_id . "'>";
+    echo "<div class='d-flex justify-content-between card-width'>";
+    echo "<span class=''> $fname $lname </span>";
+    echo "<div class='d-flex justify-content-between frind-card card-buttons-area' id='confirm" . $user_id . "'>";
     // the first user ID is guaranteed to be unique because it is the sender ID and the recipient ID is fixed 
     if ($accepted) {
         // $loggedUser = $_SESSION["data"]["user_id"];
-        echo "<button class = 'btn btn-primary btn-sm pfont add-button-width' onclick='sendConfirm(" . $user_id . ", " . $uid . ", false)'> Remove friend </button> ";
-        echo /*html*/"<a href='/messenger.php?sender=$user_id&recipient=$uid'>Message</a>";
+        echo "<button class = 'btn btn-primary btn-sm pfont icon-button' onclick='sendConfirm(" . $user_id . ", " . $uid . ", false)'><i class='fa-solid fa-user-slash white-font'></i></button> ";
+        echo /*html*/"<a href='/messenger.php?sender=$user_id&recipient=$uid' class='icon-button'><button class='btn btn-primary btn-sm pfont icon-button'><i class='fa-solid fa-message'></i></button></a>";
     } else {
-        echo "<button class='btn btn-primary btn-sm pfont add-button-width' onclick='sendConfirm(" . $user_id . ", " . $uid . ", true)'> Accept </button> ";
-        echo "<button class='btn btn-primary btn-sm pfont add-button-width' onclick='sendConfirm(" . $user_id . ", " . $uid . ", false)'> Deny </button> ";
+        echo "<button class='btn btn-primary btn-sm pfont icon-button' onclick='sendConfirm(" . $user_id . ", " . $uid . ", true)'> <i class='fa-solid fa-check whitefont'></i> </button> ";
+        echo "<button class='btn btn-primary btn-sm pfont icon-button' onclick='sendConfirm(" . $user_id . ", " . $uid . ", false)'> <i class='fa-solid fa-xmark'></i> </button> ";
     }
     echo "</div>";
     echo "</div>";
