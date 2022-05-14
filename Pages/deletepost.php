799c06acce5518744fee2fa200fbdf1472c0be83
iyas salah <191034@ppu.edu>
2022-05-15 00:56:52 +0300

    style the update and delete temp pages

--- Pages/deletepost.php
+++ Pages/deletepost.php
@@ -6,19 +6,32 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather+Sans:wght@500&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/7f83ce35c1.js" crossorigin="anonymous"></script>
     <style>
-        <?php include '../Styles/popups.css'; ?>
+        <?php
+        include '../Styles/popups.css';
+        include '../Styles/standards.css';
+
+        ?>
     </style>
 </head>
 
 <body>
-
-    <?php
-    // echo print_r($_POST);
-    require_once('../models/delete-post.php');
-    deletePost($_POST['id']);
-    ?>
-    <a href="./profile.php">Back to profile page</a>
+    <section class="main-section d-flex justify-items-center">
+        <?php
+        // echo print_r($_POST);
+        require_once('../models/delete-post.php');
+        deletePost($_POST['id']);
+        ?>
+        <button class="btn btn-primary"><a class="white-font icon-button" href="./profile.php">Back to profile page</a></button>
+    </section>
+    
 </body>
 
 </html>
