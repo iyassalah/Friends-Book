0ac6c4c1c6ebb2b5304bcba956c2e057a8ef7f94
iyas salah <191034@ppu.edu>
2022-05-17 22:34:49 +0300

    add render post comment

--- models/get-Comments.php
+++ models/get-Comments.php
@@ -1,8 +1,8 @@
 <?php
-function getPostComments($uid): mysqli_result
+function getPostComments($pid): mysqli_result
 {
     require_once('../helpers/db.php');
-    $uid = mysqli_real_escape_string(db(), $uid);
+    $pid = mysqli_real_escape_string(db(), $pid);
     $query = 
     "SELECT
     users.fname AS fname,
@@ -11,52 +11,12 @@ function getPostComments($uid): mysqli_result
     comments.content as content,
     comments.date_added AS date_added
     FROM 
-    comments join users ON comments.commenter_id = users.comment_id
+    comments join users ON comments.commenter_id = users.user_id
+    WHERE post_id = $pid
     ORDER BY comments.date_added DESC;";
-//     $query =
-//         "SELECT
-//     users.fname AS fname,
-//     users.lname AS lname,
-//     users.image AS user_pfp,
-//     users.username AS username,
-//     posts.post_timestamp AS postdate,
-//     posts.content AS content,
-//     posts.image AS image,
-//     like_count.like_count,
-//     like_count.liked_by_user,
-//     posts.author_id,    
-//     posts.post_id
-// FROM
-//     posts,
-//     users,
-//     (
-//     SELECT
-//         COUNT(likes.post_id) AS `like_count`,
-//         unliked.post_id,
-//         SUM(CASE WHEN user_id=$uid THEN 1 ELSE 0 END) AS `liked_by_user`
-//     FROM
-//         likes
-//     RIGHT JOIN posts AS `unliked`
-//     ON
-//         likes.post_id = unliked.post_id
-//     GROUP BY
-//         unliked.post_id
-// ) AS `like_count`
-// WHERE
-//     posts.author_id IN(
-//     SELECT CASE WHEN
-//         friendships.friend1_id = $uid THEN friendships.friend2_id WHEN friendships.friend2_id = $uid THEN friendships.friend1_id
-// END AS friend
-// FROM
-//     friendships
-// WHERE
-//     (friend1_id = $uid OR friend2_id = $uid) AND accepted = 1
-// ) AND users.user_id = posts.author_id  AND posts.post_id = like_count.post_id
-// ORDER BY postdate DESC;";
-
-
+    
     $result = mysqli_query(db(), $query);
-    // require_once('../templates/post.php');
+    require_once('../templates/comment-card.php');
 
     return $result;
 }
--- templates/comment-card.php
+++ templates/comment-card.php
@@ -1,8 +1,8 @@
 <?php
 function renderComments($post_id)
 {
-    include '../models/get-Comments.php';
-    include 'show-comment.php';
+    require_once '../models/get-Comments.php';
+    require_once 'show-comment.php';
     echo "<div class='d-flex flex-column flex-wrap flex-width'>";
     $result = getPostComments($post_id);
     while ($row = mysqli_fetch_assoc($result)) {
